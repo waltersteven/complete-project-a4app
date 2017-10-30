d4dca333419371f6191d0245434feac5b9408c7d
Walter Pariona <wltrst97@gmail.com>
2017-10-30 13:59:10 -0500

    Components & Navbar created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+//donde creamos clases y decoradores
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'this page was made by Walter ';
 }
