4b68ead5c623d3cfdd8a2252cc52ee080e2ce2fd
Walter Pariona <wltrst97@gmail.com>
2017-10-30 15:05:34 -0500

    list of recipes, ingredients and enhanced interface

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe','This is simply a test','http://www.seriouseats.com/recipes/assets_c/2016/03/20160309-peanut-sweet-potato-kale-coconut-soup-stew-19-thumb-1500xauto-430284.jpg'),
+    new Recipe('A test recipe','This is simply a test','http://www.seriouseats.com/recipes/assets_c/2016/03/20160309-peanut-sweet-potato-kale-coconut-soup-stew-19-thumb-1500xauto-430284.jpg'),
+    new Recipe('A test recipe','This is simply a test','http://www.seriouseats.com/recipes/assets_c/2016/03/20160309-peanut-sweet-potato-kale-coconut-soup-stew-19-thumb-1500xauto-430284.jpg')
+  ]; //an array or Recipes
 
   constructor() { }
 
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,5 @@
+export class Ingredient {
+    constructor(public name:string, public amount:number) {} //shortcut instead of creating variable and instanciated in constructor.
+    
+    
+}
