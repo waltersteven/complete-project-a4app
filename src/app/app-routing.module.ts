2dee60bb9f6a62db476c3cc60f5d922a04689ea1
Walter Pariona Sanchez <wltrst97@gmail.com>
2018-02-19 17:33:43 -0500

    Adding child routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, Router } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'recipes', component: RecipesComponent },
+    { path: 'recipes', component: RecipesComponent, children: [
+        { path: '', component: RecipeStartComponent },
+        { path: ':id', component: RecipeDetailComponent} // dynamic segment added after /recipes/ 
+    ] },
     { path: 'shopping-list', component: ShoppingListComponent },
 ];
 
